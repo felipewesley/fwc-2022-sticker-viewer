c34cb1657f59266d3238027204a297061760ebbb
Felipe Basso <fe.wesleybasso@gmail.com>
2022-10-12 22:24:38 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fwc-2022-sticker-viewer';
+}
