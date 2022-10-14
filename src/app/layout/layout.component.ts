ba2d49816d6e3e1d4eb47c713fe8faf04f999c88
Felipe Basso <fe.wesleybasso@gmail.com>
2022-10-13 23:19:05 -0300

    application layout standalone

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,9 +1,14 @@
 import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+import { SharedModule } from "app/shared";
 
 @Component({
     selector: 'app-layout',
     templateUrl: './layout.component.html',
-    styleUrls: ['./layout.component.scss']
+    styleUrls: ['./layout.component.scss'],
+    standalone: true,
+    imports: [SharedModule, RouterModule]
 })
 export class LayoutComponent {
 
