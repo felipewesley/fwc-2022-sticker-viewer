b51503e61eb2d99064aa0b1e543d2046577cc0f1
Felipe Basso <fe.wesleybasso@gmail.com>
2022-10-13 23:29:34 -0300

    services - main e detalhe components

--- src/app/modules/main/detalhe/detalhe.component.ts
+++ src/app/modules/main/detalhe/detalhe.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 
+import { MainDetalheService } from "./services/detalhe.service";
+
 @Component({
     selector: 'app-detalhe',
     templateUrl: './detalhe.component.html',
-    styleUrls: ['./detalhe.component.scss']
+    styleUrls: ['./detalhe.component.scss'],
+    providers: [MainDetalheService]
 })
 export class MainDetalheComponent implements OnInit {
 
--- src/app/modules/main/main.component.ts
+++ src/app/modules/main/main.component.ts
@@ -3,10 +3,13 @@ import { FormControl } from "@angular/forms";
 
 import { TipoBuscaEnum } from "app/dominio/enums/tipo-busca.enum";
 
+import { MainService } from "./services/main.service";
+
 @Component({
     selector: 'app-main',
     templateUrl: './main.component.html',
-    styleUrls: ['./main.component.scss']
+    styleUrls: ['./main.component.scss'],
+    providers: [MainService]
 })
 export class MainComponent implements OnInit {
 
