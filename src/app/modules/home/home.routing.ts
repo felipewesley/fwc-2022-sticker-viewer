77de678feb54498ff9b14915b610a8f7366f1f52
Felipe Basso <fe.wesleybasso@gmail.com>
2022-10-15 15:36:46 -0300

    home routing - entry point

--- src/app/modules/home/home.routing.ts
+++ src/app/modules/home/home.routing.ts
@@ -1,9 +1,12 @@
 import { Routes } from "@angular/router";
 
+import { HomeComponent } from "./home.component";
+
 export const homeRoutes: Routes = [
 
     {
-        path: ''
+        path: '',
+        component: HomeComponent
     }
 
 ];
