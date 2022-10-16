9cea70854760d822f10b56e57da20c4b0b4f013d
Felipe Basso <fe.wesleybasso@gmail.com>
2022-10-15 22:26:03 -0300

    método pesquisar - pesquisa component

--- src/app/modules/pesquisa/pesquisa.component.ts
+++ src/app/modules/pesquisa/pesquisa.component.ts
@@ -1,23 +1,61 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { MatDrawer } from "@angular/material/sidenav";
+
+import { NotificacaoService } from "app/core/notificacao";
 
 import { PesquisaService } from "./services/pesquisa.service";
 import { PesquisaFormService } from "./services/pesquisa.form.service";
+import { TipoPesquisa } from "app/dominio/types/search.type";
 
 @Component({
     selector: 'app-pesquisa',
     templateUrl: './pesquisa.component.html',
     styleUrls: ['./pesquisa.component.scss'],
-    providers: [PesquisaService,
-    PesquisaFormService]
+    providers: [PesquisaService, PesquisaFormService]
 })
 export class PesquisaComponent implements OnInit {
 
     public loading = true;
 
-    constructor() { }
+    @ViewChild('drawer', { static: true }) private _drawer: MatDrawer;
+
+    constructor(
+        private _router: Router,
+        private _activatedRoute: ActivatedRoute,
+        private _notificacao: NotificacaoService
+    ) { }
 
     ngOnInit(): void {
         
-        // setTimeout(() => this.loading = false, 5000);
+    }
+
+    public pesquisar(destino: TipoPesquisa): void {
+
+        const target: TipoPesquisa[] = [];
+
+        switch (destino) {
+
+            case 'pais':
+                target.push('pais');
+                break;
+            case 'estadio':
+                target.push('estadio');
+                break;
+            case 'jogador':
+                target.push('jogador');
+                break;
+        }
+
+        const url = target.pop();
+
+        if (!url) {
+
+            this._notificacao.erro(`Tipo de pesquisa inválido`);
+            return;
+        }
+
+        this._router.navigate([url], { relativeTo: this._activatedRoute });
+        return;
     }
 }
