01201f4b804a219370f51d1b0fa9a5d78690965b
Felipe Basso <fe.wesleybasso@gmail.com>
2022-10-16 22:48:36 -0300

    layout estadio start component

--- src/app/modules/pesquisa/estadio/start/start.component.ts
+++ src/app/modules/pesquisa/estadio/start/start.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+
+import { NotificacaoService } from "app/core/notificacao";
+
+import { EstadioService } from "../services/estadio.service";
 
 @Component({
     selector: 'app-estadio-start',
@@ -7,9 +12,25 @@ import { Component, OnInit } from "@angular/core";
 })
 export class EstadioStartComponent implements OnInit {
 
-    constructor() { }
+    public pesquisa = new FormControl('');
+
+    constructor(
+        private _service: EstadioService,
+        private _notificationService: NotificacaoService
+    ) { }
 
     ngOnInit(): void {
         
+    }
+
+    public pesquisar(): void {
+
+        const valor = this.pesquisa.value;
+
+        if (!valor) {
+
+            this._notificationService.erro(`Um código válido de estádio deve ser informado`);
+            return;
+        }
     }
 }
