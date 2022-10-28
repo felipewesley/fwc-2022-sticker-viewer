dba80b5d8fd56b7befdd62097154234375e41e9e
Felipe Basso <fe.wesleybasso@gmail.com>
2022-10-28 00:35:18 -0300

    verificações adicionais ao executar script build

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -3,21 +3,44 @@ if [ ! -d "$TEMP_DIR" ]; then
     mkdir $TEMP_DIR
 fi
 
-# Clear temp directory
-rm -rf $TEMP_DIR/*
+CAN_CONTINUE=1
 
-mv ../assets $TEMP_DIR
-mv ../*.js $TEMP_DIR
-mv ../*.txt $TEMP_DIR
-mv ../*.html $TEMP_DIR
-mv ../*.css $TEMP_DIR
-
-CONF_FILE=$TEMP_DIR/*.conf.js
-if test -f "$CONF_FILE"; then
-    echo "yes"
-    mv $TEMP_DIR/*.conf.js ../
+if [[ $(ls $TEMP_DIR | wc -l) -gt "0" ]]; then
+    CAN_CONTINUE=0
 fi
 
-# git add .
-# git commit -m "Old build removed"
-# git push
+if [ "$CAN_CONTINUE" -eq "1" ]
+then
+
+    echo "Encontrando build atual..."
+    mv ../assets $TEMP_DIR
+    mv ../*.js $TEMP_DIR
+    mv ../*.txt $TEMP_DIR
+    mv ../*.html $TEMP_DIR
+    mv ../*.css $TEMP_DIR
+
+    # Save config files
+    echo "Restaurando arquivos de configuração (.js)..."
+    for i in `find $TEMP_DIR -type f -name *.conf.js`; do mv $i ../; done
+
+    # Remove tmp data
+    echo "Excluindo build..."
+    sh ./temp-clear.sh
+
+    # Commit changes
+    # echo "Commitando alterações..."
+    # git add .
+    # git commit -m "Old build removed"
+    # git push
+
+    echo "Exclusão de build concluído."
+
+else
+
+    echo "Não foi possível remover o build atual."
+    echo "Diretório TEMP não está vazio."
+    echo "Tente sh temp-clear.sh"
+
+fi
+
+echo "Finalizado."
