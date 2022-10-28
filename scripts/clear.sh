ad07fc5ffafaa9acc316fdfbdd4eb153761ccfd6
Felipe Basso <fe.wesleybasso@gmail.com>
2022-10-27 23:45:48 -0300

    update clear script

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -1,9 +1,16 @@
-rm -rf ../assets
-rm -f ../*.js
-rm -rf ../*.txt
-rm -rf ../*.html
-rm -rf ../*.css
+mkdir ../temp
 
-git add .
-git commit -m "Old build removed"
-git push
+mv ../assets ../temp
+rm ../*.js ../temp
+rm ../*.txt ../temp
+rm ../*.html ../temp
+rm ../*.css ../temp
+
+CONF_FILE=../temp/*.conf.js
+if test -f "$CONF_FILE"; then
+    mv ../temp/*.conf.js ../
+fi
+
+# git add .
+# git commit -m "Old build removed"
+# git push
