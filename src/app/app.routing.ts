5bc406be603de35b482370f032afb5e9388fe6ad
Felipe Basso <fe.wesleybasso@gmail.com>
2023-10-29 14:35:23 -0300

    app routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,27 +4,32 @@ import { LayoutComponent } from "./layout/layout.component";
 
 export const appRouting: Routes = [
 
-    // {
-    //     path: '',
-    //     pathMatch: 'full',
-    //     redirectTo: 'home'
-    // },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'redirect-to-pesquisa',
+    pathMatch: 'full',
+    redirectTo: 'pesquisa'
+  },
 
-    {
-        path: '',
-        component: LayoutComponent,
-        children: [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
 
-            // {
-            //     path: 'home',
-            //     loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
-            // },
-            {
-                path: '',
-                loadChildren: () => import('./modules/pesquisa/pesquisa.module').then(m => m.PesquisaModule)
-            }
+      {
+        path: 'home',
+        loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+      },
+      {
+        path: 'pesquisa',
+        loadChildren: () => import('./modules/pesquisa/pesquisa.module').then(m => m.PesquisaModule)
+      }
 
-        ]
-    }
+    ]
+  }
 
 ];
